9f529bbd40fc03a4d1deca8da0e77a2830b340af
Jonata Serpa <jonataserpa@gmail.com>
2023-03-08 08:39:42 -0300

    create project home page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Link rel="stylesheet" href="/">
+        Home
+      </Link>
+    </div>
+  )
+}
